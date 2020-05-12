4d98f5e3f8766bcad6c42472d23d226b58599150
Christopher Hall <hsw@bitmark.com>
2020-05-12 13:52:18 +0800

    relocate generated files
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-pub mod rpc;
-pub mod rpc_grpc;
+// all library exports
+
 pub mod tls_certificate;
 pub mod macaroon_data;
+
+#[allow(warnings)]
+#[path = "../protos/rpc_grpc.rs"]
+pub mod rpc_grpc;
+
+#[allow(warnings)]
+#[path = "../protos/rpc.rs"]
+pub mod rpc;
